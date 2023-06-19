d5fd3686d846656880e69e7e8b971ece4eb653eb
Kaden Fairchild <fairman766@gmail.com>
2023-06-19 12:39:43 -0700

    Recent Changes

--- src/Tile.java
+++ src/Tile.java
@@ -9,4 +9,38 @@ public abstract class Tile {
     public abstract boolean isTileOccupied();
 
     public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+        EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied(){
+            return false;
+        }
+
+        @Override
+        public Piece getPiece(){
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+        Piece pieceOnTile;
+        OccupiedTile(int tileCoordinate, Piece pieceOnTile){
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied(){
+            return true;
+        }
+
+        @Override
+        public Piece getPiece(){
+            return this.pieceOnTile;
+        }
+    }
 }
