2a8f44eb8ceebebcbe9cc4a5838b5a82d2a4b5ab
Kaden Fairchild <fairman766@gmail.com>
2023-06-19 12:28:59 -0700

    Initial commit

---
+++ src/Tile.java
@@ -0,0 +1,12 @@
+public abstract class Tile {
+
+    int tileCoordinate;
+
+    Tile(int tileCoordinate){
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+}
